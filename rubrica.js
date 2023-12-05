908fc62f10de3affaf7f83487cbb514bbab37571
Lleanza7 <lucaleanza92@hotmail.com>
2023-12-05 22:07:21 +0100

    completamento struttura html

---
+++ rubrica.js
@@ -0,0 +1,7 @@
+function apriModale (){
+    document.getElementById('containerPopup').style.display='flex';
+}
+
+function chiudiModale (){
+    document.getElementById('containerPopup').style.display='none';
+}
